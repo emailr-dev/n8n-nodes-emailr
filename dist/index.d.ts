540c16b4b8ebf4bf71792f19d0ce406ceabcd49c
saeed <saeedmshdi@gmail.com>
2026-09-24 08:50:28 -0700

    chore: keep public sync limited to tracked package sources

--- dist/index.d.ts
+++
@@ -1,3 +0,0 @@
-export { EmailrApi } from "./credentials/EmailrApi.credentials";
-export { Emailr } from "./nodes/Emailr/Emailr.node";
-export { EmailrTrigger } from "./nodes/Emailr/EmailrTrigger.node";
--- dist/nodes/Emailr/Emailr.node.d.ts
+++
@@ -1,5 +0,0 @@
-import type { IExecuteFunctions, INodeExecutionData, INodeType, INodeTypeDescription } from "n8n-workflow";
-export declare class Emailr implements INodeType {
-    description: INodeTypeDescription;
-    execute(this: IExecuteFunctions): Promise<INodeExecutionData[][]>;
-}
